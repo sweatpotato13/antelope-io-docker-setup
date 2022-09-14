922c3d5a9b310556ae7faa330fb74d5220db975c
Cute_Wisp <sweatpotato13@gmail.com>
2022-09-14 09:46:14 +0900

    feat: 🎸 change version to leap v3.1.0

--- remove.sh
+++ remove.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo rm -rf ./nodeos-data-volume/nodeos-data/data/blocks/blocks.index ./nodeos-data-volume/nodeos-data/data/blocks/reversible ./nodeos-data-volume/nodeos-data/data/state  
+sudo rm -rf ./nodeos-data-volume/nodeos-data/data/blocks/blocks.log ./nodeos-data-volume/nodeos-data/data/blocks/blocks.index ./nodeos-data-volume/nodeos-data/data/blocks/reversible ./nodeos-data-volume/nodeos-data/data/state ./nodeos-data-volume/nodeos-data/config/protocol_features  
